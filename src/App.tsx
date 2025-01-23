2fa710b053f7e97673b5c22cbf12478734a07f5c
Havock48 <adit.github@gmail.com>
2025-01-23 23:21:20 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { lazy, Suspense } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router"
 import { useAuthListener } from "./hooks/useAuthListener";
 
-const Home = lazy(() => import('./pages/Home'));
-const Login = lazy(() => import('./pages/auth/Login'));
-const AuthLayout = lazy(() => import('./pages/auth/AuthLayout'));
-const Register = lazy(() => import('./pages/auth/Register'));
-const ShoppingList = lazy(() => import('./pages/ShoppingList'));
-const PrivateRoute = lazy(() => import('./components/PrivateRoute'));
-const RestrictToAuth = lazy(() => import('./components/RestrictToAuth'));
+const Home = lazy(() => import('@/pages/Home'));
+const Login = lazy(() => import('@/pages/auth/Login'));
+const AuthLayout = lazy(() => import('@/pages/auth/AuthLayout'));
+const Register = lazy(() => import('@/pages/auth/Register'));
+const ShoppingList = lazy(() => import('@/pages/ShoppingList'));
+const PrivateRoute = lazy(() => import('@/components/PrivateRoute'));
+const RestrictToAuth = lazy(() => import('@/components/RestrictToAuth'));
 
 function App() {
   useAuthListener() // Check if user is logged in
